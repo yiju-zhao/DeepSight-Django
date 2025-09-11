21f5f8dc58aad22ea4f2f2fcf7ebb5394b24da0b
Eason <29761446+yiju-zhao@users.noreply.github.com>
2025-09-11 15:38:31 -0400

    add load dotenv

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -9,6 +9,9 @@ For development-specific settings, see development.py
 import os
 from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
