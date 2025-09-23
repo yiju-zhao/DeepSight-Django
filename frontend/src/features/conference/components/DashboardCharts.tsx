6a5ce91f9583e10c196b3d0de14106d458a7a1b9
Eason Zhao <29761446+yiju-zhao@users.noreply.github.com>
2025-09-23 14:06:37 -0400

    decrease the margin of chart container

--- frontend/src/features/conference/components/DashboardCharts.tsx
+++ frontend/src/features/conference/components/DashboardCharts.tsx
@@ -445,8 +445,8 @@ const GeographicCollaborationNetwork = memo(({ data, isLoading }: { data: ForceG
         <Maximize2 className="w-5 h-5 text-gray-600" />
       </button>
       <NetworkChart
-        width={460}
-        height={510}
+        width={520}
+        height={520}
         fontSize={12}
       />
     </div>
