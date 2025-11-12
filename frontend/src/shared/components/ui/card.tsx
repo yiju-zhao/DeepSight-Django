3211f5418c694072e54e65a4fbd0eb2bec36018d
Eason Zhao <29761446+yiju-zhao@users.noreply.github.com>
2025-11-12 14:47:01 -0500

    fix import path

--- frontend/src/shared/components/ui/card.tsx
+++ frontend/src/shared/components/ui/card.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { cn } from '@/shared/utils/cn';
+import { cn } from '@/shared/utils/utils';
 
 const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
