c8418350071e505cebf6250f407fa8a5a5fb6729
Eason Zhao <29761446+yiju-zhao@users.noreply.github.com>
2025-11-20 10:30:59 -0500

    refactor: remove dashboard components and update their exports

--- frontend/src/features/dashboard/components/index.ts
+++ frontend/src/features/dashboard/components/index.ts
@@ -1,7 +1,2 @@
 // Dashboard components index
-export { default as ReportsSection } from './ReportsSection';
-export { default as PodcastsSection } from './PodcastsSection';
-export { default as ConferenceSection } from './ConferenceSection';
-export { default as DashboardActions } from './DashboardActions';
-export { default as EmptyState } from './EmptyState';
 export { default as LoadingState } from './LoadingState';
