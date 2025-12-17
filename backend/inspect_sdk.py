ad186e7ecca5e8e4e6ab530275a86b9b1c3b4b9e
Eason Zhao <29761446+yiju-zhao@users.noreply.github.com>
2025-12-17 16:28:48 -0500

    chore: remove temporary inspection scripts and add example directory to gitignore

--- backend/inspect_sdk.py
+++
@@ -1,8 +0,0 @@
-from copilotkit import CopilotKitSDK, CopilotKitRemoteEndpoint
-
-print("--- CopilotKitSDK Doc ---")
-print(CopilotKitSDK.__doc__)
-print("\n--- CopilotKitRemoteEndpoint Doc ---")
-print(CopilotKitRemoteEndpoint.__doc__)
-print("\n--- CopilotKitSDK.__init__ annotations ---")
-print(CopilotKitSDK.__init__.__annotations__)
