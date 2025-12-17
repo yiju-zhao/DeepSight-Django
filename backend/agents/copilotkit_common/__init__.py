4f165e4ad52cf53dd947e5853ec69daf84b86676
Eason Zhao <29761446+yiju-zhao@users.noreply.github.com>
2025-12-17 13:12:05 -0500

    fix import error

--- backend/agents/copilotkit_common/__init__.py
+++ backend/agents/copilotkit_common/__init__.py
@@ -3,11 +3,9 @@
 
 from .auth import verify_django_session, get_notebook_config
 from .base_server import create_agent_server
-from .utils import setup_django
 
 __all__ = [
     "verify_django_session",
     "get_notebook_config", 
     "create_agent_server",
-    "setup_django",
 ]
